4c8b68cdb743dd336498b76d8538d0e902d1d7e5
Aanand Mishra <69287620+Aanandmishra2020@users.noreply.github.com>
2020-08-08 17:55:37 +0545

    Mille To Meter Conversion App

--- Unit Conversion App.py
+++ Unit Conversion App.py
@@ -5,7 +5,7 @@ print("Welcome To Aanand Mishra's Unit Conversion App")
 mile = float(input("\nEnter The Value(in miles):"))
 
 # Converting Miles Into Meter
-meter = round(mile*1609.34,2)
+meter = mile*1609.34
 
 # Printing In Meters
 print(f'\nThe Value In Meter Is: {meter}')
