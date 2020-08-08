dde6efca2848c370f05d758ab7e86bf3849b321b
Aanand Mishra <69287620+Aanandmishra2020@users.noreply.github.com>
2020-08-08 17:35:44 +0545

    Miles to Meters conversion app

---
+++ Unit Conversion App.py
@@ -0,0 +1,14 @@
+# Welcome Message
+print("Welcome To Aanand Mishra's Unit Conversion App")
+
+# Input In Miles
+mile = float(input("\nEnter The Value(in miles):"))
+
+# Converting Miles Into Meter
+meter = mile*1609.34
+
+# Printing In Meters
+print(f'\nThe Value In Meter Is: {meter}')
+
+# Thanks Message
+print("\nThanks, For Using Aanand Mishra's Unit Conversion App.")
