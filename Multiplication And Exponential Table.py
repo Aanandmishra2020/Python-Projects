337b2d4f8d0fe7f44cd39d8ebcbf943debb3ca6c
Aanand Mishra <69287620+Aanandmishra2020@users.noreply.github.com>
2020-08-10 15:16:13 +0545

    Multiplication and exponential table app

---
+++ Multiplication And Exponential Table.py
@@ -0,0 +1,38 @@
+# Welcome Message
+print("Welcome to the Aanand Mishra's Multiplication And Exponential Table App")
+
+# Input Any Number
+num = round(float(input("\nEnter any Number:")), 2)
+
+# Multiplication Table Message
+print(f'\nThe multiplication table of {num} is displayed below:')
+
+# Printing Multiplication Table
+print(str(num)+' * '"1"+' = '+str(num*1))
+print(str(num)+' * '"2"+' = '+str(num*2))
+print(str(num)+' * '"3"+' = '+str(num*3))
+print(str(num)+' * '"4"+' = '+str(num*4))
+print(str(num)+' * '"5"+' = '+str(num*5))
+print(str(num)+' * '"6"+' = '+str(num*6))
+print(str(num)+' * '"7"+' = '+str(num*7))
+print(str(num)+' * '"8"+' = '+str(num*8))
+print(str(num)+' * '"9"+' = '+str(num*9))
+print(str(num)+' * '"10"+' = '+str(num*10))
+
+# Exponential Table Message
+print(f'\nThe Exponential table of {num} is displayed below:')
+
+# Printing Exponential Table
+print(str(num)+' ** '"1"+' = '+str(round(num**1, 2)))
+print(str(num)+' ** '"2"+' = '+str(round(num**2, 2)))
+print(str(num)+' ** '"3"+' = '+str(round(num**3, 2)))
+print(str(num)+' ** '"4"+' = '+str(round(num**4, 2)))
+print(str(num)+' ** '"5"+' = '+str(round(num**5, 2)))
+print(str(num)+' ** '"6"+' = '+str(round(num**6, 2)))
+print(str(num)+' ** '"7"+' = '+str(round(num**7, 2)))
+print(str(num)+' ** '"8"+' = '+str(round(num**8, 2)))
+print(str(num)+' ** '"9"+' = '+str(round(num**9, 2)))
+print(str(num)+' ** '"10"+' = '+str(round(num**10, 2)))
+
+# Thanks Message
+print("\nThanks, For Using Aanand Mishra's Multiplication And Exponential Table App.")
