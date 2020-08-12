bd9a9abff6aff54a37ce7dc32e2837a3c022897b
Aanand Mishra <69287620+Aanandmishra2020@users.noreply.github.com>
2020-08-12 13:59:49 +0545

    Grade Sorter App

--- Grade Sorter app.py
+++ Grade Sorter app.py
@@ -3,10 +3,11 @@ print("Welcome to the grade sorter app")
 
 # Taking Input
 grade = []
-grade.append(int(input("\nPlease Enter Your Grade:")))
-grade.append(int(input("Please Enter Your Grade:")))
-grade.append(int(input("Please Enter Your Grade:")))
-grade.append(int(input("Please Enter Your Grade:")))
+grade.append(int(input("\nPlease Enter Your First Grade:")))
+grade.append(int(input("Please Enter Your Second Grade:")))
+grade.append(int(input("Please Enter Your Third Grade:")))
+grade.append(int(input("Please Enter Your Fourth Grade:")))
+grade.append(int(input("Please Enter Your Fifth Grade:")))
 
 # Displaying List
 print("\nYour grades are "+str(grade))
@@ -18,10 +19,10 @@ grade.sort(reverse=True)
 print("\nYour grades from highest to lowest are: "+str(grade))
 
 # Displaying Lowest And Highest Grades
-grade1 = grade[-1]
-print("\nLowest grade: "+str(grade1))
-grade2 = grade[0]
-print("Highest grade: "+str(grade2))
+lowest_grade = grade[-1]
+print("\nLowest grade: "+str(lowest_grade))
+highest_grade = grade[0]
+print("Highest grade: "+str(highest_grade))
 
 # Deleting Lowest Two Grades
 del grade[-1]
