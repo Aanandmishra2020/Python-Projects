b870308b34c7af01590517b1abbdba40c7433505
Aanand Mishra <69287620+Aanandmishra2020@users.noreply.github.com>
2020-08-27 12:52:43 +0545

    Add files via upload

---
+++ Different datatypes.py
@@ -0,0 +1,49 @@
+# Title
+print("\t\t\tDifferent Datatypes\n")
+
+# Creating Variables of All types
+
+# Creating list of strings
+list_str = ['15', '10', '100', '45']
+# Creating list of integer
+list_int = [15, 10, 100, 45]
+# Creating list of float
+list_float = [15.1, 10.09, 100.45, 45.78]
+
+# Displaying 'list_str' Datatype
+print("The datatype of 'list_str' is:", type(list_str))
+
+# Displaying elements inside the list
+print("The elements inside list are:", list_str)
+
+# Displaying Datatype of elements inside list
+print("The datatype of elements inside list is:", type(list_str[0]))
+
+# Displaying 'list_int' Datatype
+print("\nThe datatype of 'list_int' is:", type(list_int))
+
+# Displaying elements inside the list
+print("The elements inside list are:", list_int)
+
+# Displaying Datatype of elements inside list
+print("The datatype of elements inside list is:", type(list_int[0]))
+
+# Displaying 'list_float' Datatype
+print("\nThe datatype of 'list_float' is:", type(list_float))
+
+# Displaying elements inside the list
+print("The elements inside list are:", list_float)
+
+# Displaying Datatype of elements inside list
+print("The datatype of elements inside list is:", type(list_float[0]))
+
+# Sorting message
+print('\nThe lists will now be sorted')
+
+# Sorting 'list_str' variable
+list_str.sort()
+print("\nThe sorted list of variable (list_str) is:", list_str)
+
+# Sorting 'list_int' variable
+list_int.sort()
+print("The sorted list of variable (list_int) is:", list_int)
